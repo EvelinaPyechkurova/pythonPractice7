02eb483530760e2344635cf75fc84eb6ee638e2a
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2024-03-19 23:01:56 +0200

    Added empty input and output functions

---
+++ app/io/input.py
@@ -0,0 +1,11 @@
+# Function to input text from the console
+def input_from_console():
+    pass
+
+# Function to read from a file using Python's built-in capabilities
+def read_from_file_builtin():
+    pass
+
+# Function to read from a file using the pandas library
+def read_from_file_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,9 @@
+# output.py
+
+# Function to output text to the console
+def output_to_console():
+    pass
+
+# Function to write to a file using Python's built-in capabilities
+def write_to_file_builtin():
+    pass
